0d0b0353f00070bf43d442352e08b8fc90bb5c17
heim-lukas <lukas27.heim@gmail.com>
2025-04-29 18:39:22 +0200

    fix date for postgresql via mapping

--- monk-mode-backend/Application/MappingProfile.cs
+++ monk-mode-backend/Application/MappingProfile.cs
@@ -5,7 +5,10 @@ using monk_mode_backend.Models;
 namespace monk_mode_backend.Application {
     public class MappingProfile : Profile {
         public MappingProfile() {
-            CreateMap<TimeBlock, TimeBlockDTO>().ReverseMap().ForMember(dest => dest.Tasks, opt => opt.Ignore());
+            CreateMap<TimeBlock, TimeBlockDTO>().ReverseMap()
+                .ForMember(dest => dest.Tasks, opt => opt.Ignore())
+                .ForMember(dest => dest.Date, opt => opt.MapFrom(src => DateTime.SpecifyKind(src.Date, DateTimeKind.Utc)));
+
             CreateMap<Friendship, FriendshipDTO>();
             CreateMap<UserTask, TaskDTO>().ReverseMap();
             CreateMap<UserTask, CreateTaskDTO>().ReverseMap();
