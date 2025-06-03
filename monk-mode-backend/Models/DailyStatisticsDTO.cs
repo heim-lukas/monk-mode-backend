eb38610238b6ea64beeadc96f8c2513af9dd7a22
swimmingbrain <braianplaku@gmail.com>
2025-06-03 17:22:32 +0200

    fix username field in statistics req

--- monk-mode-backend/Models/DailyStatisticsDTO.cs
+++ monk-mode-backend/Models/DailyStatisticsDTO.cs
@@ -8,7 +8,7 @@ namespace monk_mode_backend.Models
         public string UserId { get; set; }
         public DateTime Date { get; set; }
         public int TotalFocusTime { get; set; }
-        public string Username { get; set; }
+        public string? Username { get; set; }
         public int Xp { get; set; }
         public int Level { get; set; }
     }
