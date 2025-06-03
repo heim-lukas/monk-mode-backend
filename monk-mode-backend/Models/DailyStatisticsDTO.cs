5da08a1793cbb9809d8b19a2ba91c250819301b5
swimmingbrain <braianplaku@gmail.com>
2025-06-03 17:17:09 +0200

    add update statistic dto

--- monk-mode-backend/Models/DailyStatisticsDTO.cs
+++ monk-mode-backend/Models/DailyStatisticsDTO.cs
@@ -8,5 +8,8 @@ namespace monk_mode_backend.Models
         public string UserId { get; set; }
         public DateTime Date { get; set; }
         public int TotalFocusTime { get; set; }
+        public string Username { get; set; }
+        public int Xp { get; set; }
+        public int Level { get; set; }
     }
 } 
